999c05fae806ecbe2326270c3d867a812e436c77
Terry Liou <terry@gamer.com.tw>
2017-07-14 18:07:22 +0800

    first commit

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by 劉洧熏 on 2017/7/11.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Smashtag/TwitterUser.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterUser.swift
+//  Smashtag
+//
+//  Created by 劉洧熏 on 2017/7/11.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+import UIKit
+
+class TwitterUser: NSManagedObject {
+
+}
