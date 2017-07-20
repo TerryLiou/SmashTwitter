6512b3a996b259a020ad82f9e39a53a9f5965062
Terry Liou <terry@gamer.com.tw>
2017-07-20 18:01:49 +0800

    add searchTerm feature

---
+++ Smashtag/SearchTerm.swift
@@ -0,0 +1,13 @@
+//
+//  SearchTerm.swift
+//  Smashtag
+//
+//  Created by 劉洧熏 on 2017/7/20.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+import UIKit
+
+class SearchTerm: NSManagedObject {
+
+}
---
+++ Smashtag/SearchTermTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchTermTableViewController.swift
+//  Smashtag
+//
+//  Created by 劉洧熏 on 2017/7/20.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+import Foundation
